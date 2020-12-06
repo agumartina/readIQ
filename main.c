5941e9f2557907d3c1231dbdb18caeaaed5302a1
Agustin Martina <amartina@unc.edu.ar>
2020-12-06 20:55:02 -0300

    updated main

--- main.c
+++ main.c
@@ -3,11 +3,16 @@
 #include "include/list.h"
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
         char *filename;
         struct Node *header;
 
-        filename = "/home/sagus/CLionProjects/IQ/RMA01_9005_01_20160224T195114_245Z_02.IQ";
+        if(argc != 2){
+                printf("Se debe incluir el path de un archivo N1 compatible");
+                return 0;
+        }else{
+                filename = argv[1];
+        }
         
         /* leemos todas las cabeceras */
         header = dataN1_read(filename, 1);
@@ -15,6 +20,7 @@ int main() {
         //PrintList(header);
         //PrintDataList(header);
 
+        // TODO: make a free function
         free(header);
 
         return 0;
